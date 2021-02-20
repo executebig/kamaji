eee49e6cabfd02d636c400e893ad9a4500941cf2
CF12 <brianx0011@gmail.com>
2021-02-20 18:44:52 -0500

    Add react router, styles, swt

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
 
 import 'normalize.css'
 
+import IndexPage from './pages/index'
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Switch>
+        <Route component={IndexPage} />
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 )
